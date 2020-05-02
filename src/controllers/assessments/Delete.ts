c4b76d69e7c61a979810e6ac4967c29589b5917e
jerskisnow <jerskisnow@gmail.com>
2020-05-02 09:22:29 +0200

    Updates...

--- src/controllers/assessments/Delete.ts
+++ src/controllers/assessments/Delete.ts
@@ -12,7 +12,9 @@ export default async(msg: Message) => {
 	const pgClient = await pgPool.connect();
 
 	try {
-		await pgClient.query('UPDATE suggestions SET status = $1::text WHERE message = $2::text', ['Deleted', msg.id]);
+		const result = await pgClient.query('SELECT id FROM suggestions WHERE message = $1::text', [msg.id]);
+		if (result.rows.length === 1)
+			await pgClient.query('UPDATE suggestions SET status = $1::text WHERE message = $2::text', ['Deleted', msg.id]);
 	} finally {
 		pgClient.release();
 	}
