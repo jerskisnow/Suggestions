0f66c2fc89969b27d4529fddd0f1ebdae01a630c
jerry blaauw <jerskisnow@gmail.com>
2020-08-22 09:10:11 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ import { ShardingManager } from 'discord.js';
 import cliColors from './structures/CLIColors';
 
 const manager = new ShardingManager('app.js', {
-	token: process.env.CLIENT_TOKEN
+    token: process.env.CLIENT_TOKEN,
+    totalShards: 'auto'
 });
 
 console.log(cliColors.FgCyan + `\n ██████╗ ██████╗ ██████╗ ███████╗██████╗ ███████╗███╗   ██╗ ██████╗ ██╗    ██╗
@@ -22,5 +23,5 @@ console.log(cliColors.FgCyan + `\n ██████╗ ██████╗ �
 
 console.log(cliColors.FgBlue + "\n---=[Loading Shards...]=---" + cliColors.Reset);
 
-manager.spawn();
+manager.spawn().catch(ex => console.error(`[ERROR/SHARD] ${ex}`));
 manager.on('shardCreate', shard => console.log(`${cliColors.FgCyan}>> Shard ${cliColors.FgYellow + shard.id + cliColors.FgCyan} has been launched.`));
