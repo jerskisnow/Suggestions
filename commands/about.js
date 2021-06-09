4ae8a3b824eee5abb4baa5ed6771f9b2a09fb3e0
BuildTools <jerskisnow@gmail.com>
2021-06-09 16:50:49 +0200

    meanwhile

--- commands/about.js
+++ commands/about.js
@@ -1,28 +1,35 @@
 const { botCache } = require('../structures/cache')
-const { MessageEmbed } = require('discord.js')
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js')
+const config = require('../config')
 
 botCache.commands.set('about', {
     desc: 'Obtain general information about the bot.',
     exec: async function (client, interaction) {
-        const embed = new MessageEmbed()
-        embed.addField('Project - Suggestions',
-            "Suggestions is a discord bot, created by CodedSnow, that allows for perfect collaboration between members and staff members." +
-            " Members can create their ideas, a staff member can then approve, consider or reject them." +
-            " Suggestions also allows people to create a Report. This is useful for example for Gaming servers where a player needs to be reported or a support server where you can report bugs." +
-            "\n\nYou can invite Suggestions to your own server by clicking [here](https://top.gg/bot/566616056165302282/invite/).",
-        false)
-        embed.addField('Voting',
-            "Voting for Suggestions is a great way of supporting our project." +
-            " Voting for Suggestions will make it appear higher in de botlists, making it grow even faster" +
-            "\n\nYou can vote for suggestions by clicking [here](https://top.gg/bot/566616056165302282/vote).",
-        false)
-        embed.addField('Donating',
-            "Don't feel obligated to donate! It isn't a requirement but it helps us to maintenance our projects including Suggestions.\n\n" +
-            "If you decided to donate contact jerskisnow#8214 if you want to receive a premium rank, based on the amount you donated, on the official CodedSnow discord server!" +
-            " When you received a premium rank you'll receive some extra perks and you'll be thanked gratefully!\n\n" +
-            "You can go to our [wiki](https://github.com/jerskisnow/Suggestions/wiki/Donating) in order to find more information about donating including the payment details.",
-        false)
+        const row = new MessageActionRow().addComponents(
+            new MessageButton()
+                .setURL('https://top.gg/bot/566616056165302282/invite/')
+                .setLabel('Invite')
+                .setEmoji('🤖')
+                .setStyle('LINK'),
+            new MessageButton()
+                .setURL('https://github.com/jerskisnow/Suggestions/wiki/Donating')
+                .setLabel('Donate')
+                .setEmoji('💰')
+                .setStyle('LINK'),
+            new MessageButton()
+                .setURL('https://top.gg/bot/566616056165302282/vote')
+                .setLabel('Vote')
+                .setEmoji('📰')
+                .setStyle('LINK'),
+        )
 
-        await interaction.reply({ embeds: [embed] })
+        const embed = new MessageEmbed()
+        embed.setColor(config.embedColor.b)
+        embed.setTitle('About Suggestions')
+        embed.setDescription("Suggestions is a discord bot, created by CodedSnow (jerskisnow), that allows for perfect collaboration between members and staff members." +
+        " Members can submit their ideas, a staff member can then approve, consider or reject them." +
+        " Suggestions also offers support for reports, polls and a lot more!\n\nConsider taking a look at the buttons below this message for more information and how to support us.")
+
+        await interaction.reply({ embeds: [embed], components: [row] })
     }
 })
--- listeners/interaction.js
+++ listeners/interaction.js
@@ -1,7 +1,9 @@
 const { botCache } = require('../structures/cache')
 const { getFromCache } = require('../structures/cache')
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js')
+const config = require('../config')
 
-module.exports = async function(client, interaction) {
+module.exports = async function (client, interaction) {
     if (!interaction.isCommand()) return;
 
     if (botCache.commands.has(interaction.commandName)) {
@@ -10,7 +12,18 @@ module.exports = async function(client, interaction) {
         if (obj.isPremium) {
             const data = await getFromCache(interaction.guildID)
             if (!data.isPremium) {
-                interaction.reply('That command is for premium servers only.')
+                const row = new MessageActionRow().addComponents(new MessageButton()
+                    .setURL('https://github.com/jerskisnow/Suggestions/wiki/Donating')
+                    .setLabel('Donating')
+                    .setEmoji('💰')
+                    .setStyle('LINK'))
+
+                const embed = new MessageEmbed()
+                embed.setColor(config.embedColor.r)
+                embed.setTitle('Premium Command')
+                embed.setDescription("The command you tried to use is only for premium servers. See the button below for more information.")
+
+                await interaction.reply({ embeds: [embed], components: [row] })
                 return
             }
         }
